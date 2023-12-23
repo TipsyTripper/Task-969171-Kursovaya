fe94bb5692b907a50074a4ae96f68a5d5a2fce19
Eliza <149193972+TipsyTripper@users.noreply.github.com>
2023-12-23 15:07:17 +0300

    Delete src/main directory

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static Dice dices = new Dice();
-
-    public static void main(String[] args) {
-        dices.setVisible(true);
-        dices.setResizable(false);
-    }
-}
